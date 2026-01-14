319ca865668d4f4b6eef8f075437a7a841f26f60
Krzysztof <drkrzysztofzieba@gmail.com>
2026-01-15 00:27:59 +0100

    main fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ import rentRoutes from "./routes/rentRoutes.js";
 
 const app: Express = express();
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: 'https://csr-frontend-zq6v.onrender.com/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
